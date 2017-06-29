d5f5e699b3db0606d3d77c231154621d693ffe7e
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2017-06-29 16:26:34 +0200

    VectorIndexSelectionCast does not work for scalar images!

--- h5stat.py
+++ h5stat.py
@@ -14,19 +14,21 @@ argv = sys.argv
 
 f = h5py.File(argv[1], 'r')
 
-print("Min\tMax\tMean\tStd\tVariance\tSum\tName\tSize")
+print("c\tMin\tMax\tMean\tStd\tVariance\tSum\tName\tSize")
 
 for name in f: # http://docs.h5py.org/en/latest/quick.html#groups-and-hierarchical-organization
     D = f[name]
     d = sitk.GetImageFromArray(np.moveaxis(np.squeeze(D), 0, -1)) # https://docs.scipy.org/doc/numpy/reference/generated/numpy.squeeze.html
     stat = sitk.StatisticsImageFilter() # no procedural interface like sitk.Normalize(d) https://itk.org/SimpleITKDoxygen/html/classitk_1_1simple_1_1NormalizeImageFilter.html#details  https://itk.org/SimpleITKDoxygen/html/namespaceitk_1_1simple.html#a603f415c1d5f0475aff976d8d784dba7
 
-#    print(d)
-    for i in range(d.GetNumberOfComponentsPerPixel()):
-        print(i)
-        stat.Execute(sitk.VectorIndexSelectionCast(d, i)) # http://insightsoftwareconsortium.github.io/SimpleITK-Notebooks/Python_html/20_Expand_With_Interpolators.html # see resampler: https://itk.org/SimpleITKDoxygen/html/Python_2ImageRegistrationMethod2_8py-example.html
-        print("%f\t%f\t%f\t%f\t%f\t%f\t%s\t" # http://stackoverflow.com/questions/3264828/change-default-float-print-format
+    for i in range(d.GetNumberOfComponentsPerPixel()): # VectorIndexSelectionCast does not work for scalar images!
+        if(d.GetNumberOfComponentsPerPixel() > 1):
+            stat.Execute(sitk.VectorIndexSelectionCast(d, i)) # http://insightsoftwareconsortium.github.io/SimpleITK-Notebooks/Python_html/20_Expand_With_Interpolators.html # see resampler: https://itk.org/SimpleITKDoxygen/html/Python_2ImageRegistrationMethod2_8py-example.html
+        else:
+            stat.Execute(d)
+        print("%d\t%f\t%f\t%f\t%f\t%f\t%f\t%s\t" # http://stackoverflow.com/questions/3264828/change-default-float-print-format
               % (
+                  i,
                   stat.GetMinimum(),
                   stat.GetMaximum(),
                   stat.GetMean(),
