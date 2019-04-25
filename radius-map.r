0f19417599a4121965641c690495d4e16d7f2b11
Richard Beare <Richard.Beare@gmail.com>
2019-04-26 08:38:18 +1000

    Remove some of the test code.

--- radius-map.r
+++ radius-map.r
@@ -99,23 +99,8 @@ main <- function() {
     	{
   	stop("Missing Parameters: <binary-input> <seed-output> <wth-output>")
 	}
-
-    dt <- ReadImage(args[1], "sitkFloat32")
-
     s <- speedimage(args[1])
     WriteImages(s, args[2])
-    
-    # White top hat
-
-    wth <- WhiteTopHat(dt)
-    #wth <- wth > 0
-
-    wtha <- as.array(wth)
-    wtha <- wtha[wtha > 0]
-    hist(wtha, 50)
-    
-    gc()
-    WriteImage(wth, args[3])
 }
 
 main()
